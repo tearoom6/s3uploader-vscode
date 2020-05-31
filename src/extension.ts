3f8c9c41a9ebce2923202af19a592e11872e375f
Tomohiro Murota <tomohiro.murota@gmail.com>
2020-06-01 03:24:09 +0900

    Add test implementations of commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const uploadCopiedFileAndPasteLink = async () => {
+  const clipboardText = await vscode.env.clipboard.readText()
+  vscode.window.showInformationMessage(`uploadCopiedFileAndPasteLink: ${clipboardText}`)
+}
+
+const uploadSelectedFileAndInsertLink = async () => {
+  await vscode.commands.executeCommand('copyFilePath')
+  const clipboardText = await vscode.env.clipboard.readText()
+  vscode.window.showInformationMessage(`uploadSelectedFileAndInsertLink: ${clipboardText}`)
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "s3uploader-vscode" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('s3uploader-vscode.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from s3uploader-vscode!');
-  });
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('s3uploader-vscode.uploadCopiedFileAndPasteLink', () => uploadCopiedFileAndPasteLink()),
+    vscode.commands.registerCommand('s3uploader-vscode.uploadSelectedFileAndInsertLink', () => uploadSelectedFileAndInsertLink()),
+  )
 }
 
 // this method is called when your extension is deactivated
